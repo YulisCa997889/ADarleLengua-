34d8aeced1fcd2149fa366230cdf338d13b36c6a
YulisCa997889 <yuliscanate02@gmail.com>
2024-07-21 11:50:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navbarMenu = document.querySelector('.navbar ul');
-
-    menuIcon.addEventListener('click', function() {
-        navbarMenu.classList.toggle('show');
-    });
-});
